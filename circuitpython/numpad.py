96adb22e45f4756514c53d3996a322a1d4e832c6
Jozsef Frigo <frigojozsef@gmail.com>
2024-09-15 11:57:11 +0200

    WIP: numpad uses board.DISPLAY

--- circuitpython/numpad.py
+++ circuitpython/numpad.py
@@ -1,7 +1,23 @@
+import board
+import displayio
+import terminalio
+from adafruit_display_text import bitmap_label as label
 from layer import Layer
+
 class Numpad(Layer):
     def __init__(self, context):
         Layer.__init__(self, context, (50, 30, 10))
+        text_group = displayio.Group()
+        font = terminalio.FONT
+        for i in range(5):
+            line = label.Label(
+                font,
+                text = " ",
+                anchor_point = (0,0),
+                anchored_position = (0, 12 * i)
+            )
+            text_group.append(line)
+        self.text_group = text_group
 
     def keyEvent(self, key_event):
         macropad = self.context.macropad
@@ -52,10 +68,11 @@ class Numpad(Layer):
 
     def display(self):
         self.context.macropad.display_sleep = False
-        text_lines = self.context.text_lines
+        text_lines = self.text_group
         text_lines[0].text = "       NUMPAD  brite"
         text_lines[1].text = "   7      8      9"
         text_lines[2].text = "   4      6      7"
         text_lines[3].text = "   1      2      3"
         text_lines[4].text = "   0    ENTER    ,"
+        board.DISPLAY.root_group = text_lines
         self.context.pixels.fill(self.color)
